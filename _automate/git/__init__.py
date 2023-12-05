b0113e2b6b1e65d56482bb4e0245931746ccac44
goFrendiAsgard <gofrendiasgard@gmail.com>
2023-12-05 08:38:58 +0700

    local: Update on 2023-12-05T08:38:54.748702

--- _automate/git/__init__.py
+++ _automate/git/__init__.py
@@ -1,9 +1,7 @@
-from . import add
 from . import commit
 from . import fetch
 from . import pull
 
-assert add
 assert commit
 assert fetch
 assert pull
--- _automate/git/commit.py
+++ _automate/git/commit.py
@@ -2,7 +2,7 @@ from typing import Any
 from zrb import runner, CmdTask
 from .._group import noto_git_group
 from ..config import NOTO_MACHINE_NAME
-from .add import git_add
+from .fetch import git_fetch
 import os
 import datetime
 
@@ -14,15 +14,19 @@ def _git_commit_cmd(*args: Any, **kwargs: Any):
     now = datetime.datetime.now().isoformat()
     message = f'{NOTO_MACHINE_NAME}: Update on {now}'
     return [
-        'set +e',
-        f'git commit -m "{message}"'
+        'if git diff-index --quiet HEAD --; then',
+        '    echo "No changes to commit."',
+        'else',
+        '    git add .',
+        f'    git commit -m "{message}"',
+        'fi'
     ]
 
 
 git_commit = CmdTask(
     name='commit',
     group=noto_git_group,
-    upstreams=[git_add],
+    upstreams=[git_fetch],
     cwd=NOTO_DIR,
     cmd=_git_commit_cmd()
 )
