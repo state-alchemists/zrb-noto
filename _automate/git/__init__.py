4772a6747628bc6ef0a1d5a41b01b2d0b1665f0e
goFrendiAsgard <gofrendiasgard@gmail.com>
2023-12-05 09:01:43 +0700

    local: Update on 2023-12-05T09:01:39.095165

--- _automate/git/__init__.py
+++ _automate/git/__init__.py
@@ -1,7 +1,9 @@
 from . import commit
 from . import fetch
 from . import pull
+from . import push
 
 assert commit
 assert fetch
 assert pull
+assert push
---
+++ _automate/git/push.py
@@ -0,0 +1,27 @@
+from typing import Any
+from zrb import runner, CmdTask
+from .._group import noto_git_group
+from ..config import NOTO_GIT_REMOTE_NAME
+from ._helper import get_current_branch
+from .commit import git_commit
+import os
+
+CURRENT_DIR = os.path.dirname(__file__)
+NOTO_DIR = os.path.dirname(os.path.dirname(CURRENT_DIR))
+
+
+def _git_push_cmd(*args: Any, **kwargs: Any):
+    current_branch = get_current_branch()
+    return [
+        f'git push -u {NOTO_GIT_REMOTE_NAME} {current_branch}'
+    ]
+
+
+git_push = CmdTask(
+    name='push',
+    group=noto_git_group,
+    upstreams=[git_commit],
+    cwd=NOTO_DIR,
+    cmd=_git_push_cmd()
+)
+runner.register(git_push)
